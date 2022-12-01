5afad651bad0c86d0cba0a5cd4405c74a8a57579
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-01 22:29:56 +0530

    Update yourPlayground.js

---
+++ Learning-Javascript/yourPlayground.js
@@ -0,0 +1,26 @@
+//console.log('hello world')
+//console.log("Ayan Masood")
+
+//variables
+name = 'Peter pan'
+//console.log(name)
+
+sentence = 'How are you doing today, nice to see you'
+
+//console.log(sentence)
+
+//operators
+//fruit = prompt('What is your fav fruit?')
+
+//console.log(fruit)
+
+food = Number(prompt('Cost of the food')) 
+tipPercentage = Number(prompt('percentage to tip')) / 100
+tipAmount = food * tipPercentage
+total = food + tipAmount 
+
+console.log('tip amount:' , tipAmount)
+console.log('total:', total)
+
+
+// User input 
