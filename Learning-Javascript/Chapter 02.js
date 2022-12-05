1372fdb8d7307ce2e236d950bffba03069faf92a
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-05 23:32:27 +0530

    Update Chapter 02.js

--- Learning-Javascript/Chapter 02.js
+++ Learning-Javascript/Chapter 02.js
@@ -8,4 +8,38 @@
  boolean -- true/false
 */
 
+/* math operations 
+Multiply * 
+Divide / 
+Exponents **
+Modulo/Remainder % 
+Add +
+Subtract - 
+*/
 
+/*Math methods 
+Floor - Rounds down
+Cell - Rounds up
+Random - gives you number between 0 and 1
+*/
+
+/* Variables
+Const - The value cannot be changed
+let - The value can be changed
+var - The value can be changed (not recommended)
+*/
+
+// Baby weather app (Conditionals)
+// if rain - "grab your umbrella"
+// else "wear your sunglasses"
+
+let weather = prompt('How is the weather')
+
+if(weather == 'rain') {
+    console.log('Grab your umbrealla')
+} else {
+    console.log('Wear your sunglasses')
+}
+
+//Conditionals opertaors 
+// ==, ===, >, <, <=, >=, !=, !==
