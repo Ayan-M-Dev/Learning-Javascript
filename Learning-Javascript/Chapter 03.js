dc0cfc664a969ff49495265eca72b694c42cf33e
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-06 21:51:55 +0530

    Update Chapter 03.js

---
+++ Learning-Javascript/Chapter 03.js
@@ -0,0 +1,19 @@
+// //Functions
+// function sayMyName(name){
+//     console.log(name)
+// }
+
+// sayMyName('Ayan')
+
+// function sayMyName2(name){
+//     console.log(name)
+// }
+
+// sayMyName2('Kevin')
+
+function greeting(name){
+    greet = 'hi ' + ', Nice to meet you'
+    console.log(greet)
+}
+
+greeting('Ayan')
