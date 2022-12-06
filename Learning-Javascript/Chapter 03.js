18db4bcdf5ef62e73ac93f4f1ca7dc85da780684
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-06 23:06:32 +0530

    Chapter 03

--- Learning-Javascript/Chapter 03.js
+++ Learning-Javascript/Chapter 03.js
@@ -11,9 +11,46 @@
 
 // sayMyName2('Kevin')
 
-function greeting(name){
-    greet = 'hi ' + ', Nice to meet you'
-    console.log(greet)
+// function greeting(name){
+//     greet = 'Hi ' + name + ', Nice to meet you'
+//     console.log(greet)
+// }
+
+// greeting('Ayan')
+
+// function greeting(name){
+//     greet = `Hi ${name}, Nice to meet you!`
+//     console.log(greet)
+// }
+
+// greeting('Ayan')
+
+function sum(a, b){
+    return a + b
 }
 
-greeting('Ayan')
+// num1 = sum(1,2)
+// console.log(num1)
+
+
+function calculateFoodTotal(food, tip) { 
+    const tipPercentage = tip / 100
+    const tipAmount = food * tipPercentage
+    const total = sum(food , tipAmount)
+    return total 
+}
+
+console.log(calculateFoodTotal(300, 20))
+
+
+//ES^
+//Arrow Functions =>
+//With explicit function
+// const sumArrow = (a, b) => {
+//     return a + b
+// }
+
+//With implicit return function
+const sumArrow = (a, b) => a + b 
+
+console.log(sumArrow(10,50))
