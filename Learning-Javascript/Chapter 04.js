4ddd04c1e168d24c585bc944443b1dcdd17e337b
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-07 18:38:55 +0530

    Update Chapter 04.js

--- Learning-Javascript/Chapter 04.js
+++ Learning-Javascript/Chapter 04.js
@@ -1,2 +1,18 @@
 //Arrays
 groceries = ['Banana' , 'Apple' , 'Orange' , 'Pear']
+// console.log(groceries)
+
+//Grab the third index
+// console.log(groceries[3])
+
+//Array Methods
+groceries.push('Watermelon')
+// console.log(groceries)
+
+// //Array slice
+// console.log(groceries.slice(0,2))
+
+// console.log(groceries.slice(1,4))
+
+//Array Methods (slice, push, indexOf, length)
+console.log(groceries.indexOf('Orange'))
