85047df20ace19f93066b2f279f3f0149a595674
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-09 17:08:03 +0530

    Update Chapter 06.js

--- Learning-Javascript/Chapter 06.js
+++ Learning-Javascript/Chapter 06.js
@@ -14,13 +14,33 @@ const fruits = ['Banana' , 'Apple' , 'Orange' , 'Pear','Banana' , 'Apple' , 'Ora
 //     console.log(fruit)
 // }
 
-const numbers = [1, 2, 3, 4, 5, 6]
+// const numbers = [1, 2, 3, 4, 5, 6]
 
 //Sum up all numbers in array
-for (let i=0; i< numbers.length; i++) {
-    console.log(numbers[i])
-}
+// for (let i=0; i< numbers.length; i++) {
+//     console.log(numbers[i])
+//}
 
 // for (const sum of numbers) {
-//     console.log(sum)
+//      console.log(sum * 2)
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6]
+// let result = []
+
+// for (const number of numbers) {
+//     result.push(number * 2)
+// }
+
+// console.log(result)
+
+const double = (numbers) => {
+    let result = []
+    for (const number of numbers) {
+        result.push(number * 2)
+    }
+
+    return result
 }
+
+console.log(double([1,2,3,4,5,6]))
