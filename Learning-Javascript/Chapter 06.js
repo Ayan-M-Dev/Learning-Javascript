ea217d59218e087b211b195d19a990f20cfa726a
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-09 16:46:10 +0530

    Update Chapter 06.js

---
+++ Learning-Javascript/Chapter 06.js
@@ -0,0 +1,11 @@
+const fruits = ['Banana' , 'Apple' , 'Orange' , 'Pear','Banana' , 'Apple' , 'Orange' , 'Pear','Banana' , 'Apple' , 'Orange' , 'Pear',]
+// console.log(fruits[0])
+// console.log(fruits[1])
+// console.log(fruits[2])
+// console.log(fruits[3])
+
+//For loop
+
+for (let i=0; i < fruits.length; i++){
+    console.log(i, fruits[i])
+}
