a43d8253054b8c52e7520a1f1afdc595859d6f88
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-15 23:43:14 +0530

    Update Chapter 08.js

--- Learning-Javascript/Chapter 08.js
+++ Learning-Javascript/Chapter 08.js
@@ -6,3 +6,14 @@
 [1, 2, 3, 4].map(number => {
     console.log(number)
 })
+
+//OR
+
+[1, 2, 3, 4].map(number => console.log(number))
+
+
+
+let result = [1, 2, 3, 4].map(number => number * 2)
+console.log(result)
+
+
