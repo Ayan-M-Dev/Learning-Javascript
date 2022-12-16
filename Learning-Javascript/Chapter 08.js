abad32325060905374055bfd96d6bf48de4b375c
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-16 21:54:18 +0530

    Update Chapter 08.js

--- Learning-Javascript/Chapter 08.js
+++ Learning-Javascript/Chapter 08.js
@@ -3,17 +3,23 @@
 //filter
 //reduce
 
-[1, 2, 3, 4].map(number => {
-    console.log(number)
-})
+// [1, 2, 3, 4].map(number => {
+//     console.log(number)
+// })
 
-//OR
+// //OR
 
-[1, 2, 3, 4].map(number => console.log(number))
+// [1, 2, 3, 4].map(number => console.log(number))
 
 
 
-let result = [1, 2, 3, 4].map(number => number * 2)
-console.log(result)
+// let result = [1, 2, 3, 4].map(number => number * 2)
+// console.log(result)
+
+const doubleMap = (numbers) => {
+    return numbers.map(number => number * 2)
+}
+
+console.log(doubleMap([1, 2, 3, 4]))
 
 
