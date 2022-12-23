eaacf99e844c18cad65b7de646b339527f812ad0
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-23 17:17:22 +0530

    Chapter 09

--- Learning-Javascript/Chapter 09.js
+++ Learning-Javascript/Chapter 09.js
@@ -14,10 +14,27 @@ tittle.innerHTML = `<p> ${message} </p>`
 
 tittle.style.backgroundColor = 'red'
 
-let redDiv = document.getElementById('red')
-let yellowDiv = document.getElementById('yellow')
-let greenDiv = document.getElementById('green')
+const squares = document.querySelectorAll('.colorSquare')
 
-redDiv.onclick = () => console.log('red')
-yellowDiv.onclick = () => console.log('yellow')
-greenDiv.onclick = () => console.log('green')
+
+/* forEach LOOP */
+
+const timesClicked = {'red': 0, 'yellow': 0, 'green': 0}
+squares.forEach(square => {
+  square.onclick = () => {
+    timesClicked[square.value] += 1
+    square.innerText = timesClicked[square.value]
+  }
+})
+
+function clearScores() {
+    timesClicked.red = 0
+    timesClicked.yellow = 0
+    timesClicked.green = 0
+    
+    square.forEach(square => {
+        square.innerText = 0
+})
+
+const clearGameBtn = document.getElementById('clear-game')
+clearGameBtn.onclick = () => clearScores()
