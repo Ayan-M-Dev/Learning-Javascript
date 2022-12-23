1dd509049e6ec001536a484841e9611621b0ecd8
Ayan Masood <ayan.m.dev@gmail.com>
2022-12-23 16:07:58 +0530

    Chapter 09

--- Learning-Javascript/Chapter 09.js
+++ Learning-Javascript/Chapter 09.js
@@ -2,4 +2,16 @@ console.log('hello')
 
 let tittle = document.getElementById('tittle')
 
-console.log ('before: ',tittle.innerText)
+console.log ('before: ', tittle.innerText)
+
+let message = 'Goodbye my lover!'
+
+tittle.innerText = message;
+
+console.log('after: ', tittle.innerText)
+
+tittle.innerHTML = `<p> ${message} </p>`
+
+tittle.style.backgroundColor = 'red'
+
+let red = document.getElementById('red')
